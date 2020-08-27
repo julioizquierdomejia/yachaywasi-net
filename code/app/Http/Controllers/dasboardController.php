b86fb701a2e48287dee851fb64e2ca1c6ff7d713
Miguel AB <abm_8249@outlook.com>
2020-08-27 00:30:44 -0500

    validar cursos segun usuario

--- code/app/Http/Controllers/dasboardController.php
+++ code/app/Http/Controllers/dasboardController.php
@@ -19,9 +19,14 @@ class dasboardController extends Controller
     	$userAuth = User::findOrFail((int) $id);
     	$docentes = $todos = User::join('role_user', 'role_user.user_id', '=', 'users.id')->where('role_id', 3)->get();
         //$cursos = Course::all();//$userAuth->courses;
-        $cursos = Course::join('degree_level_courses','degree_level_courses.course_id','courses.id')->select('degree_level_courses.*', 'courses.name as course_name', 'courses.user_id')
+        if (\Auth::user()->roles->first()->name == 'lector') {
+            $cursos = Course::join('degree_level_courses','degree_level_courses.course_id','courses.id')->select('degree_level_courses.*', 'courses.name as course_name', 'courses.user_id')
             ->where('degree_level_courses.degree_level_id', $id)
             ->get();
+        } else {
+            $cursos = Course::all();
+        }
+        
         $degreeLevelUser = DegreeLevelUser::where('user_id',$id)->get();
 
     	//llamamos a todos los docentes relacionados con este colegio
