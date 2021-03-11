7ea4fbecca563b70834656a616ae8e4e123146b5
Miguel AB <abm_8249@outlook.com>
2021-03-10 23:47:42 -0500

    popup de horas

--- resources/views/admin/schedule/assign.blade.php
+++ resources/views/admin/schedule/assign.blade.php
@@ -1,5 +1,16 @@
 @extends('layouts.app')
 @section('content')
+@php
+	$days = [
+		'Lunes',
+		'Martes',
+		'Miércoles',
+		'Jueves',
+		'Viernes',
+		'Sábado',
+		'Domingo',
+	]
+@endphp
 <div class="schedule">
 	<h5>{{$title}}</h5>
 	<div class="card">
@@ -26,7 +37,7 @@
 									@endif
 									@endforeach
 									<div class="buttons">
-										<button class="btn btn-primary btn-assign" type="button">Asignar</button>
+										<button class="btn btn-primary btn-assign" data-toggle="modal" data-target="#modalAssign" type="button"><i class="fa fa-plus pr-2"></i> Asignar</button>
 									</div>
 								</div>
 							</div>
@@ -40,5 +51,35 @@
 			</ul>
 		</div>
 	</div>
+</div>
+<div class="modal fade" id="modalAssign" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">{{$title}}</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <ul>
+        	@for ($i = 0; $i < 7; $i++)
+        	<li>
+        		<input type="checkbox" name="" id="hour_{{$i}}">
+        		<label for="hour_{{$i}}">{{$days[$i]}}, 8 am</label>
+        	</li>
+        	@endfor
+        </ul>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">
+          <i class="fas fa-times-circle mr-2" style='font-size: 14px;'></i>Cerrar
+        </button>
+        <button type="button" class="btn btn-primary btn-assign-hour">
+          <i class="fa fa-plus mr-2" style='font-size: 14px;'></i>Asignar
+        </button>
+      </div>
+    </div>
+  </div>
 </div>
 @endsection
