2747051ea0d02f9b8ccd03ecd7ef8647e8c8c31f
Miguel AB <abm_8249@outlook.com>
2021-03-18 00:50:27 -0500

    pagina de mantenimiento

--- resources/views/layouts/old_errors.blade.php
+++ resources/views/layouts/old_errors.blade.php
@@ -1,32 +1,36 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
+
 <head>
   <meta charset="utf-8" />
   <link rel="apple-touch-icon" sizes="76x76" href="./assets/img/apple-icon.png">
   <link rel="icon" type="image/png" href="./assets/img/favicon.png">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-  <!--title>{{ config('app.name', 'Yachaywasi') }}</title-->
-  <title>{{isset($title) ? $title . ' | ' : ''}} Yachaywasi</title>
+  <title>{{ config('app.name', 'Laravel') }}</title>
   <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  
+
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+  
+  
   <!-- CSS Just for demo purpose, don't include it in your project -->
  <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
   <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet" />
   <link href="{{ asset('css/paper-dashboard.css?v=2.0.1') }}" rel="stylesheet" />
+  
   <link href="{{ asset('demo/demo.css') }}" rel="stylesheet" />
-  <link href="{{ asset('css/datatables.min.css') }}" rel="stylesheet" />
 </head>
 
 <body class="">
-  <div class="wrapper">
-    @include('layouts.sidebar')
-    <div class="main-panel" style="height: 100vh;">
+  <div class="wrapper h-100">
+    <div class="main-panel" style="height: 100vh; width: 100%;">
       <!-- Navbar -->
       <nav class="navbar navbar-expand-lg navbar-absolute fixed-top navbar-transparent">
         <div class="container-fluid">
           <div class="navbar-wrapper">
+            <!-- menu hamburguesa ********** --> 
             <div class="navbar-toggle">
               <button type="button" class="navbar-toggler">
                 <span class="navbar-toggler-bar bar1"></span>
@@ -34,28 +38,20 @@
                 <span class="navbar-toggler-bar bar3"></span>
               </button>
             </div>
-            <a class="navbar-brand" href="javascript:;">Colegio - {{Auth::user()->school}}</a>
+            <!-- end menu hamburguesa ********** --> 
+            <a class="navbar-brand" href="javascript:;">Colegio Lev S. Vigotsky - Los Olivos</a>
           </div>
+          <!-- menu 3 puntitos ... ********** --> 
           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-bar navbar-kebab"></span>
             <span class="navbar-toggler-bar navbar-kebab"></span>
             <span class="navbar-toggler-bar navbar-kebab"></span>
           </button>
+          <!-- end menu 3 puntitos ... ********** --> 
+          
+          <!-- Menu Login y Registro --> 
           <div class="collapse navbar-collapse justify-content-end" id="navigation">
-            <ul class="navbar-nav">
-
-            	<li class="nav-item btn-rotate dropdown">
-                <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                  <i class="nc-icon nc-bell-55"></i>
-                  <p>
-                    <span class="d-lg-none d-md-block">Some Actions</span>
-                  </p>
-                </a>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-                  <a class="dropdown-item" href="#">Action</a>
-                </div>
-              </li>
-              
+            <ul class="navbar-nav">              
               <!-- Authentication Links -->
                 @guest
                     <li class="nav-item">
@@ -91,19 +87,18 @@
 
             </ul>
           </div>
+          <!-- end Menu Login y Registro --> 
         </div>
       </nav>
       <!-- End Navbar -->
       <div class="content">
         <div class="row">
           <div class="col-md-12">
-            <!--<h3 class="description">Your content here</h3>-->
-
             @yield('content')
           </div>
         </div>
       </div>
-      @include('layouts.footer')
+      
     </div>
   </div>
   <!--   Core JS Files   -->
@@ -121,86 +116,7 @@
   <script src="{{asset('js/plugins/bootstrap-notify.js')}}"></script>
   <!-- Control Center for Now Ui Dashboard: parallax effects, scripts for the example pages etc -->
   <script src="{{asset('js/paper-dashboard.min.js?v=2.0.1')}}" type="text/javascript"></script>
-
-  <script src="{{asset('js/javascript.js')}}"></script>
-
-  <script src="{{asset('js/datatables.js')}}"></script>
-
-
-  <!-- Script para el modal de active -->
-  <script>
-    $('#exampleModal').on('show.bs.modal', function (event) {
-      var button = $(event.relatedTarget) // Button that triggered the modal
-      var idUser = button.data('id') // Extract info from data-* attributes
-      var statusUser = button.data('status') // Extract info from data-* attributes
-      // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
-      // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
-      var modal = $(this)
-      modal.find('.modal-user-active').text('New message to ' + idUser)
-      modal.find('.modal-body #id').val(idUser)
-      modal.find('.modal-body #status').val(statusUser)
-
-      if(statusUser == 0){
-        $('#opc-active').text('Pendiente')
-      }else{
-        $('#opc-active').text('Activo')
-      }
-
-
-    })
-    var modal = $('#exampleModal')
-    $('.dropdown-item').click(function(){
-      $('#opc-active').text($(this).text());
-      modal.find('.modal-body #status').val($(this).data('index'))
-    })
-
-    $('.btn-delete-user').click(function(){
-      $('#form-delete-user').submit();
-    })
-
-    $('.btn-delete-course').click(function(){
-      $('#form_delete_coutse').submit();
-    })
-
-    $('.button-tooltip').tooltip()
-
-    $('#table_subjects').DataTable( {
-        "pagingType": "full_numbers"
-    } );
-
-    $('#table_studens').DataTable( {
-        "pagingType": "full_numbers",
-        "paging":   false,
-        "ordering": false,
-        "info":     false,
-        "dom": '<"top"i>ll<"top"flp><"clear">'
-    } );
-    var dLanguage = {
-          "sProcessing":     "Procesando...",
-          "sLengthMenu":     "Mostrar _MENU_ registros",
-          "sZeroRecords":    "No se encontraron resultados",
-          "sEmptyTable":     "Ningún dato disponible en esta tabla",
-          "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
-          "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
-          "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
-          "sInfoPostFix":    "",
-          "sSearch":         "Buscar:",
-          "sUrl":            "",
-          "sInfoThousands":  ",",
-          "sLoadingRecords": "Cargando...",
-          "oPaginate": {
-            "sFirst":    "Primero",
-            "sLast":     "Último",
-            "sNext":     "Siguiente",
-            "sPrevious": "Anterior"
-          },
-          "oAria": {
-            "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
-            "sSortDescending": ": Activar para ordenar la columna de manera descendente"
-          }
-        }
-  </script>
-
-  @yield('script')
+  
 </body>
+
 </html>
